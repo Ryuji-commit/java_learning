411679bc2aad5a4ebc05bd43a886fe17214f94fe
Ryuji-commit <56810015+Ryuji-commit@users.noreply.github.com>
2021-04-11 18:40:33 +0900

    pushed

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int x = 5;
+        int y = 10;
+        String result = "x + y = " + (x + y);
+        System.out.println(result);
     }
 }
